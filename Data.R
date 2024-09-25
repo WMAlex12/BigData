8ec88cc997b14da3c90fa6f048d9517502807555
WMAlex <karel-.-alex@hotmail.com>
2024-09-25 00:43:10 -0600

    Correccion final para presentar 25/09/2024

---
+++ Data.R
@@ -0,0 +1,3 @@
+library(datasets)
+require(graphics)
+mosaicplot(Titanic, main = "Survival on the Titanic")
---
+++ Pueba.py
@@ -0,0 +1,11 @@
+
+import numpy as np 
+import pandas as pd 
+
+print("Hola, mundo de python.")
+
+
+
+
+
+### d
